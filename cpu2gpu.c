bde32b9210ead93ae7b26b93430c0e5efa5542b1
Satoshi OHSHIMA <ohshima@cc.u-tokyo.ac.jp>
2017-09-26 11:59:02 +0900

    update cpu2gpu.c

--- cpu2gpu.c
+++ cpu2gpu.c
@@ -7,7 +7,7 @@
 int main(int argc, char **argv)
 {
   int i;
-  int myid, nprocs, ierr, provided;
+  int myrank, nprocs, ierr, provided;
   MPI_Status status;
   int N = 1000, loops;
   double time, t_min=999999.99, t_max=0.0, t_sum=0.0;
@@ -21,9 +21,10 @@ int main(int argc, char **argv)
   N = atoi(argv[1]);
   loops = atoi(argv[2]);
 
-  ierr = MPI_Init_thread(&argc,&argv,MPI_THREAD_FUNNELED,&provided);
-  if(provided!=MPI_THREAD_FUNNELED)printf("MPI_THREAD_FUNNELED is not provided.\n");
-  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  //ierr = MPI_Init_thread(&argc,&argv,MPI_THREAD_FUNNELED,&provided);
+  //if(provided!=MPI_THREAD_FUNNELED)printf("MPI_THREAD_FUNNELED is not provided.\n");
+  MPI_Init(&argc,&argv);
+  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   ierr = MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   if(nprocs!=2){
     printf("2 processes are required.\n");
@@ -31,13 +32,13 @@ int main(int argc, char **argv)
   }
 
   data = (double*)malloc(sizeof(double)*N);
-  if(myid==1){
+  if(myrank==1){
     cudaMalloc((void*)&d_data, sizeof(double)*N);
   }
 
   ierr = MPI_Barrier(MPI_COMM_WORLD);
   for(i=0; i<loops; i++){
-    if(myid==0){
+    if(myrank==0){
       time = MPI_Wtime();
       ierr = MPI_Send(data, N, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
       ierr = MPI_Recv(data, N, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &status);
@@ -45,20 +46,20 @@ int main(int argc, char **argv)
       if(time>t_max)t_max=time;
       if(time<t_min)t_min=time;
       t_sum += time;
-    }else if(myid==1){
+    }else if(myrank==1){
       ierr = MPI_Recv(d_data, N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
       ierr = MPI_Send(d_data, N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
   }
-  if(myid==0){
-    printf("TIME %d : %e (average %e msec, min %e msec, max %e msec)\n", myid, t_sum,
+  if(myrank==0){
+    printf("TIME %d : %e (average %e msec, min %e msec, max %e msec)\n", myrank, t_sum,
 	   t_sum/(double)loops*1000.0,
 	   t_min/(double)loops*1000.0,
 	   t_max/(double)loops*1000.0
 	   );
   }
 
-  if(myid==1){
+  if(myrank==1){
     cudaFree(d_data);
   }
   free(data);
